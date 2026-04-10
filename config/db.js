5f9b06211b89df00ae1a50b795896ecc8ff4c4c9
mohamedkhaledmk <khaled25256@gmail.com>
2026-04-10 18:00:55 +0200

    Database connection file

---
+++ config/db.js
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
+
+const prisma = new PrismaClient({
+  adapter,
+  log:
+    process.env.NODE_ENV === "development"
+      ? ["query", "error", "warn"]
+      : ["error"],
+});
+
+const connectDB = async () => {
+  try {
+    await prisma.$connect();
+    console.log(`Db Connected Successfully using PRISMA...`);
+  } catch (err) {
+    console.log(`DB Connection ERROR! ${err.message}`);
+    process.exit(1);
+  }
+};
+
+const disconnectDB = async () => {
+  await prisma.$disconnect();
+};
+
+export { prisma, connectDB, disconnectDB };
